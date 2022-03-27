28e76157dc079025e146cb91d8f673dc55ccf9af
Ben Halverson <benhalverson33@gmail.com>
2022-03-27 00:17:08 -0700

    added readme

---
+++ readme.md
@@ -0,0 +1,6 @@
+# setup 
+rename .env.local to .env
+
+`npm i` 
+
+`npm run dev`
--- src/utils/getCoinPrice.ts
+++ src/utils/getCoinPrice.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const getCoinPrice = async (coin: string): Promise<Response | undefined> => {
+export const getCoinPrice = async (coin: string) => {
   try {
     const coinPrice = await axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
     );
-    const data = coinPrice.data.filter((token: Response) => token.symbol === "eth");
+    const data = coinPrice.data.filter((token: any) => token.symbol === "eth");
     const eth = {
       name: data[0].name,
       price: data[0].current_price,
@@ -17,32 +17,3 @@ export const getCoinPrice = async (coin: string): Promise<Response | undefined> 
     console.error("Error getting price", error.message);
   }
 };
-
-export interface Response {
-    id?:                           string;
-    symbol?:                       string;
-    name?:                         string;
-    image?:                        string;
-    currentPrice?:                 number;
-    marketCap?:                    number;
-    marketCapRank?:                number;
-    fullyDilutedValuation?:        number;
-    totalVolume?:                  number;
-    high24H?:                      number;
-    low24H?:                       number;
-    priceChange24H?:               number;
-    priceChangePercentage24H?:     number;
-    marketCapChange24H?:           number;
-    marketCapChangePercentage24H?: number;
-    circulatingSupply?:            number;
-    totalSupply?:                  number;
-    maxSupply?:                    number;
-    ath?:                          number;
-    athChangePercentage?:          number;
-    athDate?:                      Date;
-    atl?:                          number;
-    atlChangePercentage?:          number;
-    atlDate?:                      Date;
-    roi?:                          null;
-    lastUpdated?:                  Date;
-}
