5e7b827c3b3f48769c98dd059e966cbb6f7ca20d
Ben Halverson <benhalverson33@gmail.com>
2022-04-04 22:34:14 -0700

    added gasprice function

--- src/utils/getCoinPrice.ts
+++ src/utils/getCoinPrice.ts
@@ -17,3 +17,32 @@ export const getCoinPrice = async (coin: string) => {
     console.error("Error getting price", error.message);
   }
 };
+const API_KEY = 'PWRYSUPZUUD8Q218AH834UAVW518SVWX1C';
+const ACTION = 'gasoracle';
+const BASE_URL = 'https://api.etherscan.io/api';
+const url = `${BASE_URL}?module=gastracker&action=${ACTION}&apikey=${API_KEY}`;
+const getGas = async () => {
+const response = axios.get(url);
+
+console.log('response', (await response).data);
+}
+
+const getBalance = async () => {
+  // https://api.etherscan.io/api
+  //  ?module=account
+  //  &action=balance
+  //  &address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+  //  &tag=latest
+  //  &apikey=YourApiKeyToken
+  const modules = 'account';
+  const action = 'balace';
+  const address = '0x4E488e7C4814cb502d972AaC485a17B498A75E0C';
+  const tag = 'latest';
+  const url = `${BASE_URL}?module=${modules}&action=${action}&address=${address}&tag=latest&apikey=${API_KEY}`;
+
+  const response = axios.get(url);
+  console.log('balance', (await response).data);
+}
+
+// getGas();
+getBalance()
