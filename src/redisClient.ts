20819e449a9ace6fc578f4ce7b37dc2725f3e875
Ben Halverson <benhalverson33@gmail.com>
2023-04-08 20:27:45 -0700

    refactor redis client

---
+++ src/redisClient.ts
@@ -0,0 +1,16 @@
+import Redis from "ioredis";
+
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: parseInt(process.env.REDIS_PORT || "6379"),
+});
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redisClient.on("error", (error) => {
+  console.error("Error connecting to Redis:", error);
+});
+
+export { redisClient };
